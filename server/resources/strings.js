75c71eb38f58605950a80f938084d7e040fcb2a1
Fabio Matias <fabio.matias@closer.pt>
2015-12-29 14:00:14 +0000

    success messages

--- server/resources/strings.js
+++ server/resources/strings.js
@@ -1,4 +1,8 @@
 module.exports = {
+	success: {
+		userCreated: "User created successfully",
+		userDeleted: "User deleted successfully"
+	},
 	errors: {
 		duplicateUser: "The chosen username is already taken, please choose another"
 	}
