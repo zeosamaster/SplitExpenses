5595dee70a19414f33e00a80f1de4ee2866f1f07
Fabio Matias <fabio.matias@closer.pt>
2015-12-29 15:05:14 +0000

    success and error array on response headers

--- server/controllers/http.js
+++ server/controllers/http.js
@@ -1,18 +1,20 @@
 'use strict';
 
+function setArrayHeader(res, key, value) {
+	var values = res.get(key) || [];
+	values.push(value);
+	res.append(key, values);
+}
+
 var http = {
 	sendSuccess: function (res, msg) {
-		res.locals.success = res.locals.success || [];
-		res.locals.success.push(msg);
+		setArrayHeader(res, 'success', msg);
 	},
 	sendError: function (res, msg) {
 		console.log("#", msg);
-		res.locals.error = res.locals.error || [];
-		res.locals.error.push(msg);
+		setArrayHeader(res, 'error', msg);
 	},
 	sendJson: function (res, json) {
-		res.locals.success && json.success = res.locals.success;
-		res.locals.error && json.error = res.locals.error;
 		res.json(json);
 	}
 };
