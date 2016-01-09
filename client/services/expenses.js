8cfc1d1a8c74f47391322f95753a8b2c67be7b31
zeosamaster <fabio_mcm2@hotmail.com>
2016-01-09 20:03:05 +0000

    delete expenses and payments files (will be copied later), rename "delete" to "remove"

--- client/services/expenses.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('ngSplitExpenses.expensesServices', [])
-
-.service('expensesServices', ['serverServices', function (serverServices) {
-	this.controller = '/expenses';
-}]);
--- client/services/payments.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('ngSplitExpenses.paymentsServices', [])
-
-.service('expensesServices', ['serverServices', function (serverServices) {
-	this.controller = '/payments';
-}]);
